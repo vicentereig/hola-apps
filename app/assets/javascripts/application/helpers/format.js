1c43cc562f113f4a0c444ba2e3414d858bf75278
Vicente Reig Rincon de Arellano <vicente@newrelic.com>
2014-08-10 00:48:40 -0700

    Avoid 'n,ull'

--- app/assets/javascripts/application/helpers/format.js
+++ app/assets/javascripts/application/helpers/format.js
@@ -1,4 +1,7 @@
 Ember.Handlebars.helper('format', function(value) {
+    if (Ember.isNone(value)){
+        return 0;
+    }
     var inGroupsOfThree, formatted;
 
     inGroupsOfThree = /(?=(?:...)*$)/;
