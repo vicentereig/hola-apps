4872a909346aef8e533b77556fe188f443dbd650
Vicente Reig Rincon de Arellano <vicente@newrelic.com>
2014-08-19 08:03:39 -0700

    Bump assets version

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,7 @@ module Watchtower
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.handlebars.templates_root = 'application/templates'
-    config.assets.version = '1.2'
+    config.assets.version = '1.3'
     config.assets.precompile += %w( ng_application.js )
   end
 end
