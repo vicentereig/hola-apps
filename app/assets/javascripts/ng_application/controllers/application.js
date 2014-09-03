7c654a3e8948bd1b636bac72c4a8c2b07cd20ca0
Vicente Reig Rincon de Arellano <vicente@newrelic.com>
2014-09-02 23:13:43 -0700

    Angular Version: getting my shit together.

--- app/assets/javascripts/ng_application/controllers/application.js
+++ app/assets/javascripts/ng_application/controllers/application.js
@@ -1,6 +1,15 @@
 // 1. $state sets the contents of the search box (if there's something to set fromt he query params)
 function ApplicationController($scope, $state) {
     $scope.term = $state.params.term;
+
+    this.searchTermDidChange = function(newValue, oldValue){
+        if (oldValue == newValue) {
+            return;
+        }
+        $state.goto('application.index', {term: newValue});
+    };
+
+    $scope.$watch('term', this.searchTermDidChange);
 }
 
 Watchtower.controller('ApplicationController', ['$scope', '$state', ApplicationController]);
--- app/assets/javascripts/ng_application/controllers/apps_index.js
+++ app/assets/javascripts/ng_application/controllers/apps_index.js
@@ -1,14 +1,6 @@
 // 2. we listen to $scope.term changes to trigger a search
-function AppsIndexController($scope, $state, apps) {
+function AppsIndexController($scope, apps) {
     $scope.apps = apps;
-    this.searchTermDidChange = function(newValue, oldValue){
-        if (oldValue == newValue) {
-            return;
-        }
-        $state.goto('application.index', {term: newValue});
-    };
-
-    $scope.$watch('term', this.searchTermDidChange);
 }
 
-Watchtower.controller('AppsIndexController', ['$scope', '$state', 'apps', AppsIndexController]);
+Watchtower.controller('AppsIndexController', ['$scope', 'apps', AppsIndexController]);
